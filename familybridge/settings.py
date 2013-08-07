fad439b54355c4dcac7049d34deb94bc9840d7cd
Kwan Hong Lee <kwan@redstar.com>
2013-08-07 17:57:07 -0400

    bootstrap and html5mobile boilerplate added

--- familybridge/settings.py
+++ familybridge/settings.py
@@ -12,17 +12,30 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '^o)l3eq8uaoidlw$^ec(-1bfh3r&pry^f4ci%(q6(l41shi#ky'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-TEMPLATE_DEBUG = True
+if 'DEPLOY' in os.environ and os.environ['DEPLOY'] == 'true':
+  DEPLOY = True  # only True if production (for mail settings and https)
+else:
+  DEPLOY = False
+
+# SECURITY WARNING: don't run with debug turned on in production!
+if 'DEBUG' in os.environ and os.environ['DEBUG'] == 'true':
+  DEBUG = True
+else:
+  DEBUG = False
+
+try:
+  from winedora.settings_debug import *
+except Exception as e:
+  print e
+
+TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = []
 
@@ -36,6 +49,17 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Uncomment the next line to enable admin documentation:
+    'django.contrib.admindocs',
+    #'south',
+    'debug_toolbar',
+    'sorl.thumbnail',
+    #'storages',
+    's3_folder_storage',
+    'gunicorn',
+    'compressor',
+    'djcelery',
+    'django_tables2',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -54,13 +78,14 @@ WSGI_APPLICATION = 'familybridge.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
+import dj_database_url
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {'default': dj_database_url.config()}
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
@@ -79,4 +104,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_URL = '/static/'
+DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.DefaultStorage'
+DEFAULT_S3_PATH = "media"
+STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
+STATIC_S3_PATH = "static"
+AWS_ACCESS_KEY_ID = 'AKIAJJICZZXZQAAJGRIA'
+AWS_SECRET_ACCESS_KEY = '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'
+if DEPLOY:
+  AWS_STORAGE_BUCKET_NAME = 'cdn.prod.familybridge.redstar.com'
+else:
+  AWS_STORAGE_BUCKET_NAME = 'cdn.staging.familybridge.redstar.com'
+
+MEDIA_ROOT = '/%s/' % DEFAULT_S3_PATH
+MEDIA_URL = '//s3.amazonaws.com/%s/media/' % AWS_STORAGE_BUCKET_NAME
+STATIC_ROOT = "/%s/" % STATIC_S3_PATH
+STATIC_URL = '//s3.amazonaws.com/%s/static/' % AWS_STORAGE_BUCKET_NAME
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+
+try:
+  from winedora.settings_local import *
+except Exception as e:
+  print e
+
---
+++ familybridge/settings_local.rename.py
@@ -0,0 +1,95 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = BASE_DIR + '/emails/'
+
+USE_CELERY = False
+
+# For testing real send
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_PORT = 587
+#EMAIL_HOST_USER = 'tech@vinely.com'
+#EMAIL_HOST_PASSWORD = ''
+#EMAIL_USE_TLS = True
+
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+
+import dj_database_url
+
+# customize database configuration
+DATABASES = {'default': dj_database_url.config(default='postgres://familybridge:buildbridge@localhost:5432/familybridge')}
+
+MIDDLEWARE_CLASSES = (
+    # 'johnny.middleware.LocalStoreClearMiddleware',
+    # 'johnny.middleware.QueryCacheMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+INTERNAL_IPS = ('127.0.0.1',)
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Uncomment the next line to enable admin documentation:
+    'django.contrib.admindocs',
+    'south',
+    'debug_toolbar',
+    'sorl.thumbnail',
+    #'storages',
+    's3_folder_storage',
+    'gunicorn',
+    'compressor',
+    'djcelery',
+    'django_tables2',
+)
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    BASE_DIR + '/templates',
+    BASE_DIR + "/venv/lib/python2.7/site-packages/debug_toolbar/templates/"
+)
+
+
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
+
+MEDIA_ROOT = BASE_DIR + '/sitemedia/'
+MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        # 'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+        'TIMEOUT': 500,
+        'BINARY': True,
+        'OPTIONS': {  # Maps to pylibmc "behaviors"
+            'tcp_nodelay': True,
+            'ketama': True
+        }
+    }
+}
