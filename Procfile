7aec379e45512f2caa92e53ac1692e3431227ca6
Kwan Hong Lee <kwan@redstar.com>
2013-08-13 16:06:19 -0400

    new relic, database url fixed

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: newrelic-admin run-program gunicorn familybridge.wsgi:application --bind=0.0.0.0:$PORT 
--- familybridge/settings.py
+++ familybridge/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'familybridge.wsgi.application'
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 import dj_database_url
 
-DATABASES = {'default': dj_database_url.config(default="sqlite:////familybridge.sqlite")}
+DATABASES = {'default': dj_database_url.config()}
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
